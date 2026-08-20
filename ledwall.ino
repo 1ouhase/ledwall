effa79f12b14612f6fa2b653b3b30d570762281b
1ouhase <emil.stegmann2004@gmail.com>
2026-08-20 15:35:49 +0200

    Add files via upload
    i man pooping my pants

--- ledwall.ino
+++ ledwall.ino
@@ -1,103 +1,106 @@
-#include <Adafruit_NeoPixel.h>
+#include <FastLED.h>
 
 #define LED_PIN 6
 
-#define TRIG_ONE 9
-#define ECHO_ONE 10
+#define NUM_LEDS 36
 
-#define TRIG_TWO 7
-#define ECHO_TWO 8
+unsigned long lastStateChange;
 
+CRGB colour = (255, 0, 0);
 
+CRGB leds[NUM_LEDS];
 
-#define NUM_LEDS 50
-
-//led segments
-
-Adafruit_NeoPixel strip(
-    NUM_LEDS,
-    LED_PIN,
-    NEO_GRB + NEO_KHZ800
-  );
 struct Segment {
   int start;
   int end;
-  };
-  Segment segmentOne = {0,29};
-  Segment segmentTwo = {30, 59};
- 
+  int brightness;
+  bool state;
+};
 
-// Sonic sensors
+struct Sensor {
+  int trig;
+  int echo;
+  int sensorSeg[2];
+};
+
+const int sensorAmount = 2;
+Sensor sensors[sensorAmount] = {
+  {9,10,{0,1}},
+  {7,8,{2,3}}
+};
+
+const int segmentAmount = 2;
+Segment segments[segmentAmount] = {
+  {0, 17, 0, false},
+  {18, 35, 0, false}
+};
 
 long getDistance(int trigPin, int echoPin){
 
-    digitalWrite(trigPin, LOW);
-    delayMicroseconds(2);
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
 
-    digitalWrite(trigPin, HIGH);
-    delayMicroseconds(10);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
 
-    digitalWrite(trigPin, LOW);
+  digitalWrite(trigPin, LOW);
 
-    long duration = pulseIn(echoPin, HIGH, 30000);
+  long duration = pulseIn(echoPin, HIGH, 30000);
 
-    if (duration == 0){
-      return -1;
-      }
-      return duration * 0.0343 / 2;
+  if (duration == 0){
+    return -1;
+  }
+  return duration * 0.0343 / 2;
+}
+
+void fillSegment(Segment segment, CRGB colour) {
+    for (int i = segment.start; i <= segment.end; i++) {
+        leds[i] = colour;
+        leds[i].nscale8(segment.brightness);
+    }
+}
+
+void changeStates(){
+  if(millis() - 30 < lastStateChange){
+    return;
   }
 
-  void setSegment(Segment segment, uint32_t colour){
-      for (int i = segment.start; i <= segment.end; i++) {
-          strip.setPixelColor(i, colour);
-          strip.show();
-          delay(20);
-        }
+  for(int i = 0; i<segmentAmount; i++){
+    if(segments[i].state && segments[i].brightness < 255){
+      segments[i].brightness + 5;
+      fillSegment(segments[i], colour);
     }
+    if(!segments[i].state && segments[i].brightness > 0){
+      segments[i].brightness - 5;
+      fillSegment(segments[i], colour);
+    }
+  }
+  lastStateChange = millis();
+}
+
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(sensors[0].trig, OUTPUT);
+  pinMode(sensors[0].echo, INPUT);
 
-  pinMode(TRIG_ONE, OUTPUT);
-  pinMode(ECHO_ONE, INPUT);
-
-  pinMode(TRIG_TWO, OUTPUT);
-  pinMode(ECHO_TWO, INPUT);
-
-  strip.begin();
-  strip.clear();
-
-  strip.show();
+  pinMode(sensors[1].trig, OUTPUT);
+  pinMode(sensors[1].echo, INPUT);
+  
+  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
+  
+  FastLED.clear();
+  FastLED.show();
 
   Serial.begin(9600);
-
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  changeStates();
 
-  long distanceOne = getDistance(TRIG_ONE, ECHO_ONE);
-
-  delay(30);
-
-  long distanceTwo = getDistance(TRIG_TWO, ECHO_TWO);
-
-  Serial.print("Sensor one: ");
-  Serial.print(distanceOne);
-  Serial.println(" cm");
-
-  Serial.print("Sensor two: ");
-  Serial.print(distanceTwo);
-  Serial.println(" cm");
-
-  if (distanceOne < 20) {
-    setSegment(segmentOne, strip.Color(0,255,0));
-    delay(1000);
-    if(distanceTwo< 20){
-      setSegment(segmentTwo, strip.Color(0,255,0));
-      delay(100);
-      }
+  for(int i = 0; i<sensorAmount; i++){
+    if (getDistance(sensors[i].trig, sensors[i].echo) < 20){
+        segments[i].state = true;
     }
+  }
 
-
-
+  delay(10);
 }
